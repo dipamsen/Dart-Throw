9008fca5d99a8abf8bbcad1baf9900c2c0b3c429
Fun Planet <funplanet.m4f@gmail.com>
2022-02-04 19:32:19 +0530

    score, dart

--- DartThrow.js
+++ DartThrow.js
@@ -1,33 +1,59 @@
 let SCORE = 0;
+let RELOAD = 60;
+let totalDarts = 0;
+let totalHits = 0;
 
 AFRAME.registerComponent("dart-throw", {
   init() {
     document.addEventListener("keydown", (e) => {
       if (e.key === " ") {
-        console.log("Shoot!");
         this.shoot();
       }
     });
   },
   shoot() {
-    const scene = document.querySelector("#scene");
-    const cam = document.querySelector("#player");
-    const pos = cam.getAttribute("position");
-    const dart = document.createElement("a-entity");
-    dart.className = "dart";
-    const dir = cam.object3D.getWorldDirection();
-    dart.setAttribute("out-of-bounds-destroy");
-    dart.setAttribute("position", { x: pos.x, y: pos.y, z: pos.z });
-    dart.setAttribute("material", { color: "blue" });
-    dart.setAttribute("geometry", { primitive: "sphere", radius: 0.1 });
-    dart.setAttribute("dynamic-body", { shape: "sphere", mass: 0 });
-    dart.setAttribute("velocity", dir.multiplyScalar(-10));
-    dart.addEventListener("collide", (e) => this.handleCollision(e));
-    scene.append(dart);
-    console.log(document.querySelectorAll(".dart").length);
+    if (this.canShoot()) {
+      const scene = document.querySelector("#scene");
+      const cam = document.querySelector("#player");
+      const pos = cam.getAttribute("position");
+      const dart = document.createElement("a-entity");
+      dart.className = "dart";
+      const dir = cam.object3D.getWorldDirection();
+      dart.setAttribute("out-of-bounds-destroy", {});
+      dart.setAttribute("position", { x: pos.x, y: pos.y, z: pos.z });
+      dart.setAttribute("material", { color: "blue" });
+      dart.setAttribute("geometry", { primitive: "sphere", radius: 0.1 });
+      dart.setAttribute("dynamic-body", { shape: "sphere", mass: 0 });
+      dart.setAttribute("velocity", dir.multiplyScalar(-10));
+      dart.addEventListener("collide", (e) => this.handleCollision(e));
+      scene.append(dart);
+      totalDarts++;
+      this.updateHitCount();
+      RELOAD = 60;
+    }
+  },
+  updateHitCount() {
+    const domElt = document.querySelector(".hit-miss");
+    domElt.innerText = `${totalHits}/${totalDarts}`;
+  },
+  tick() {
+    if (RELOAD > 0) RELOAD--;
+
+    const reloadBar = document.querySelector("#reload-bar");
+    reloadBar.style.backgroundColor = "red";
+    reloadBar.style.width = `${100 - (RELOAD / 60) * 100}%`;
+    if (RELOAD === 0) {
+      reloadBar.style.width = "100%";
+      reloadBar.style.backgroundColor = "green";
+    }
+  },
+  canShoot() {
+    return RELOAD === 0;
   },
   handleCollision(e) {
     if (e.detail.body.el.id !== "board") return;
+    totalHits++;
+    this.updateHitCount();
     this.addScore(10);
   },
   addScore(num) {
